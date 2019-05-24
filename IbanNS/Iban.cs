3f22c1725d57eee5f061c78ef348f778807df482
Douglas Warner Jurado Peña <douglaswarner.jp@gmail.com>
2019-05-24 19:30:50 +0200

    Añadido metodo comprobar patron, longitud. Separado metodo TrocearCC

--- IbanNS/Iban.cs
+++ IbanNS/Iban.cs
@@ -3,30 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace IbanNS
 {
-    public class Iban
-    {
+	public class ParametrosIncorrectos : ArgumentException { };
+	public class LongitudIncorrecta : ArgumentOutOfRangeException { };
 
+	public class Iban
+    {
 		protected string calcularIban(string ccc)
 		{
-			// Calculamos el IBAN
-			ccc = ccc.Trim();
-			if (ccc.Length != 20)
-			{
-				return "La CCC debe tener 20 dígitos";
-			}
-			// Le añadimos el codigo del pais al ccc
-			ccc = ccc + "142800";
+			if (!ComprobarLongitudCC(ref cc))
+				throw new LongitudIncorrecta();
+			if (!ComprobarPatronIban(cc))
+				throw new ParametrosIncorrectos();
+
+			// ES00 España
+			string ES = cc + "142800";
 
 			// Troceamos el ccc en partes (26 digitos)
-			string[] partesCCC = new string[5];
-			partesCCC[0] = ccc.Substring(0, 5);
-			partesCCC[1] = ccc.Substring(5, 5);
-			partesCCC[2] = ccc.Substring(10, 5);
-			partesCCC[3] = ccc.Substring(15, 5);
-			partesCCC[4] = ccc.Substring(20, 6);
+			string[] partesCC = new string[5];
+			TrocearCC(ES, ref partesCC);
 
 			int iResultado = int.Parse(partesCCC[0]) % 97;
 			string resultado = iResultado.ToString();
@@ -42,6 +40,32 @@ namespace IbanNS
 				restoIban = "0" + restoIban;
 
 			return "ES" + restoIban + ccc;
+		}
+		private void TrocearCC(string cc, ref string[] partesCC)
+		{
+			partesCC[0] = cc.Substring(0, 5);
+			partesCC[1] = cc.Substring(5, 5);
+			partesCC[2] = cc.Substring(10, 5);
+			partesCC[3] = cc.Substring(15, 5);
+			partesCC[4] = cc.Substring(20, 6);
+		}
+		private bool ComprobarLongitudCC(ref string cc)
+		{
+			for (int i = 0; i < cc.Length; i++)
+			{
+				if (cc[i] == ' ')
+					cc = cc.Remove(i, 1);
+			}
+			if (cc.Length != 20)
+				return false;
+			return true;
+		}
+		private bool ComprobarPatronIban(string cc)
+		{
+			string patron = @"\A[A-Z]{2}[0-9]{22}\Z";
+			Regex reg = new Regex(patron);
+
+			return reg.IsMatch(cc);
 		}
 	}
 }
